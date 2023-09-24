53c0b0a0ed855f88aba07fcd90adce85c993ed77
horacio barrios <horacioxbarrios@gmail.com>
2023-09-23 23:52:17 -0300

    tercer

--- BibliotecaCLases/Validacion.cs
+++ BibliotecaCLases/Validacion.cs
@@ -18,6 +18,7 @@ namespace BibliotecaCLases
                 }
             }
             return true;
+            /*hora hora*/
         }
         public bool ValidarDigit(string value)
         {
