bc286e765595eeeea4d814f14f6c59e0290e4409
alexisaranda1 <98905408+alexisaranda1@users.noreply.github.com>
2023-09-28 15:45:32 -0300

    separamos responsabilidades en carpetas

--- BibliotecaCLases/Administrador.cs
+++ BibliotecaCLases/Administrador.cs
@@ -3,6 +3,9 @@
     public class Administrador : Usuario
     {
 
+
+
+
         public Administrador(string nombre, string apellido, string correo, string dni, string clave)
             : base(nombre, apellido, correo,dni, clave)
         {
