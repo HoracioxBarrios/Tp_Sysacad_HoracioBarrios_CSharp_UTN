0dfafec660ae8d1a30b880bc73591b5bc11fd376
Dianyeli Yrigay <dianyrygay@gmail.com>
2023-10-02 19:17:32 -0300

    se desarrolla logica para agregar curso

--- BibliotecaCLases/Controlador/CrudCurso.cs
+++ BibliotecaCLases/Controlador/CrudCurso.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BibliotecaCLases.Modelo;
+using BibliotecaCLases.Utilidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +10,59 @@ namespace BibliotecaCLases.Controlador
 {
     public class CrudCurso
     {
-     
+        private Dictionary<int, Curso> dictCursos;
+        private string _path;
 
+        public CrudCurso()
+        {
+            dictCursos = new Dictionary<int, Curso>();
+            _path = PathManager.ObtenerRuta("Data", "DictCurso.json");
+            dictCursos = Serializador.LeerJson<Dictionary<int, Curso>>(_path);
+        }
+
+        public string Path
+        {
+            get { return _path; }
+            set { _path = value; }
+        }
+
+        public void AgregarCurso(string nombre, string codigo, string descripcion, string cupoMaximo)
+        {
+            if (int.TryParse(codigo, out int codigoCurso))
+            {
+                if (!dictCursos.ContainsKey(codigoCurso))
+                {
+                    Curso nuevoCurso = new Curso(nombre, codigo, descripcion, cupoMaximo);
+
+                    if (dictCursos != null)
+                    {
+                        dictCursos.Add(codigoCurso, nuevoCurso);
+                    }
+
+                    Serializador.ActualizarJson(nuevoCurso, codigoCurso, _path);
+                }
+                else
+                {
+                    Console.WriteLine("El código ya existe en el diccionario.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("El código no es válido como número entero.");
+            }
+        }
+
+        public void ModificarCurso(int codigo, string nombre, string descripcion, string cupoMaximo)
+        {
+        }
+
+        public void EliminarCurso(int codigo)
+        {
+        }
+
+        public Dictionary<int, Curso> ObtenerDictCursos()
+        {
+            return dictCursos;
+        }
     }
 }
--- BibliotecaCLases/Controlador/GestorCursos.cs
+++ BibliotecaCLases/Controlador/GestorCursos.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,24 @@ namespace BibliotecaCLases.Controlador
 {
     public class GestorCursos
     {
+        private CrudCurso crudCurso;
+        private string _nombre;
+        private string _codigo;
+        private string _descripcion;
+        private string _cuposDisponibles;
 
+        public GestorCursos(string nombre, string codigo, string descripcion, string cuposDisponibles)
+        {
+            crudCurso = new CrudCurso();
+            _nombre = nombre;
+            _codigo = codigo;
+            _descripcion = descripcion;
+            _cuposDisponibles = cuposDisponibles;
+        }
 
+        public void AgregarCurso(string nombre, string codigo, string descripcion, string cupoMaximo)
+        {
+            crudCurso.AgregarCurso(nombre, codigo, descripcion, cupoMaximo);
+        }
     }
 }
