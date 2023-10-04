375fea6400ba2c76f0bba79dc1892c43fc99cf4b
Dianyeli Yrigay <dianyrygay@gmail.com>
2023-10-03 22:40:14 -0300

    merge con main, resolucion de conflictos

--- BibliotecaCLases/Controlador/CrudCurso.cs
+++ BibliotecaCLases/Controlador/CrudCurso.cs
@@ -41,32 +41,62 @@ namespace BibliotecaCLases.Controlador
 
         public void AgregarCurso(string nombre, string codigo, string descripcion, string cupoMaximo)
         {
-            if (int.TryParse(codigo, out int codigoCurso))
+            int.TryParse(codigo, out int codigoCurso);
+            Curso nuevoCurso = new Curso(nombre, codigo, descripcion, cupoMaximo);
+
+            if (dictCursos != null)
             {
-                if (!dictCursos.ContainsKey(codigoCurso))
-                {
-                    Curso nuevoCurso = new Curso(nombre, codigo, descripcion, cupoMaximo);
+                dictCursos.Add(codigoCurso, nuevoCurso);
+            }
 
-                    if (dictCursos != null)
-                    {
-                        dictCursos.Add(codigoCurso, nuevoCurso);
-                    }
+            Serializador.ActualizarJson(nuevoCurso, codigoCurso, _path);
+        }
 
-                    Serializador.ActualizarJson(nuevoCurso, codigoCurso, _path);
-                }
-                else
+        public string EditarCurso(string codigo, string nombreAtributo, string nuevoValor)
+        {
+            int.TryParse(codigo, out int codigoCurso);
+            if (dictCursos.ContainsKey(codigoCurso))
+            {
+                Curso cursoExistente = dictCursos[codigoCurso];
+                // Realizar validaciones si es necesario y modificar el curso
+                switch (nombreAtributo)
                 {
-                    Console.WriteLine("El código ya existe en el diccionario.");
+                    case "Nombre":
+                        cursoExistente.Nombre = nuevoValor;
+                        break;
+                    case "Descripcion":
+                        cursoExistente.Descripcion = nuevoValor;
+                        break;
+                    case "CupoMaximo":
+                        cursoExistente.CupoMaximo = nuevoValor;
+                        break;
+                    default:
+                        return "El nombre del atributo no es válido.";
                 }
+                // Actualizar el archivo JSON con el curso modificado
+                Serializador.ActualizarJson(cursoExistente, codigoCurso, _path);
+                return "Se modificó correctamente";
+            }
+            else
+            {
+                return "El curso no existe en el diccionario.";
             }
         }
-
-        public void ModificarCurso(int codigo, string nombre, string descripcion, string cupoMaximo)
+        public string EliminarCurso(string codigo)
         {
-        }
-
-        public void EliminarCurso(int codigo)
-        {
+            int.TryParse(codigo, out int codigoCurso);
+            if (dictCursos.ContainsKey(codigoCurso))
+            {
+                // Si el curso existe en el diccionario, elimínalo
+                dictCursos.Remove(codigoCurso);
+                // Actualizar el archivo JSON sin el curso eliminado
+                Serializador.GuardarAJson(dictCursos, _path);
+                return "Se Eliminó el curso";
+            }
+            else
+            {
+                return "El curso no existe en el diccionario.";
+            }
         }
 
         public Dictionary<int, Curso> ObtenerDictCursos()
