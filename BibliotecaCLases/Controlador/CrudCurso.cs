26cb6ab8788ee9f942362fa05681f93eb41182cd
horacio barrios <horacioxbarrios@gmail.com>
2023-10-06 18:09:36 -0300

    Doc Crud Curso

--- BibliotecaCLases/Controlador/CrudCurso.cs
+++ BibliotecaCLases/Controlador/CrudCurso.cs
@@ -1,42 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using BibliotecaCLases.Modelo;
+﻿using BibliotecaCLases.Modelo;
 using BibliotecaCLases.Utilidades;
-using Newtonsoft.Json;
+
+
+
+
 
 namespace BibliotecaCLases.Controlador
 {
+
+
+    /// <summary>
+    /// Clase que gestiona la operación CRUD (Crear, Leer, Actualizar, Eliminar) de cursos.
+    /// </summary>
     public class CrudCurso
     {
         private Dictionary<int, Curso> dictCursos;
         private string _path;
 
+
+
+        /// <summary>
+        /// Constructor de la clase CrudCurso.
+        /// </summary>
         public CrudCurso()
         {
             _path = PathManager.ObtenerRuta("Data", "DictCurso.json");
             dictCursos = Serializador.LeerJson<Dictionary<int, Curso>>(_path);
         }
 
-        public string Path
-        {
-            get { return _path; }
-            set { _path = value; }
-        }
 
-        public int VerificarCodigoCurso(string codigo)
-        {
-            if (dictCursos != null)
-            {
-                if (dictCursos.Any(kv => kv.Value.Codigo == codigo))
-                {
-                    return 1;
-                }
-            }
 
-            return 0;
-        }
 
+        /// <summary>
+        /// Agrega un nuevo curso al diccionario de cursos.
+        /// </summary>
+        /// <param name="nombre">Nombre del curso.</param>
+        /// <param name="codigo">Código del curso.</param>
+        /// <param name="descripcion">Descripción del curso.</param>
+        /// <param name="cupoMaximo">Cupo máximo del curso.</param>
+        /// <param name="dia">Día del curso.</param>
+        /// <param name="horario">Horario del curso.</param>
+        /// <param name="aula">Aula del curso.</param>
         public void AgregarCurso(string nombre, string codigo, string descripcion, string cupoMaximo, string dia, string horario, string aula)
         {
             int.TryParse(codigo, out int codigoCurso);
@@ -49,6 +53,16 @@ namespace BibliotecaCLases.Controlador
             }
         }
 
+
+        /// <summary>
+        /// Edita un curso existente en el diccionario de cursos.
+        /// </summary>
+        /// <param name="codigo">Código del curso a editar.</param>
+        /// <param name="nuevoCodigo">Nuevo código del curso.</param>
+        /// <param name="nuevoNombre">Nuevo nombre del curso.</param>
+        /// <param name="nuevaDescripcion">Nueva descripción del curso.</param>
+        /// <param name="nuevoCupoMaximo">Nuevo cupo máximo del curso.</param>
+        /// <returns>Un mensaje que indica si la edición fue exitosa o si ocurrió un error.</returns>
         public string EditarCurso(string codigo, string nuevoCodigo, string nuevoNombre, string nuevaDescripcion, string nuevoCupoMaximo)
         {
             int.TryParse(codigo, out int codigoCurso);
@@ -90,20 +104,14 @@ namespace BibliotecaCLases.Controlador
             }
         }
 
-        public Curso ObtenerCursoPorCodigo(string codigo)
-        {
-            int.TryParse(codigo, out int codigoCurso);
 
-            if (dictCursos.ContainsKey(codigoCurso))
-            {
-                return dictCursos[codigoCurso];
-            }
-            else
-            {
-                return new Curso("", "", "", "");
-            }
-        }
 
+
+        /// <summary>
+        /// Elimina un curso de forma lógica, marcándolo como inactivo en el diccionario de cursos.
+        /// </summary>
+        /// <param name="curso">El curso a eliminar.</param>
+        /// <returns>Un mensaje que indica si la eliminación fue exitosa o si ocurrió un error.</returns>
         public string EliminarCurso(Curso curso)
         {
             int.TryParse(curso.Codigo, out int codigoCurso);
@@ -124,12 +132,37 @@ namespace BibliotecaCLases.Controlador
             }
         }
 
-        public Dictionary<int, Curso> ObtenerDictCursos()
+
+
+
+
+
+        /// <summary>
+        /// Verifica si un código de curso existe en el diccionario de cursos.
+        /// </summary>
+        /// <param name="codigo">El código de curso a verificar.</param>
+        /// <returns>1 si el código de curso existe, 0 si no existe.</returns>
+        public int VerificarCodigoCurso(string codigo)
         {
-            dictCursos = Serializador.LeerJson<Dictionary<int, Curso>>(_path);
-            return dictCursos;
+            if (dictCursos != null)
+            {
+                if (dictCursos.Any(kv => kv.Value.Codigo == codigo))
+                {
+                    return 1;
+                }
+            }
+
+            return 0;
         }
 
+
+
+
+        /// <summary>
+        /// Inscribe un estudiante en un curso si hay cupos disponibles.
+        /// </summary>
+        /// <param name="codigo">El código del curso en el que inscribir al estudiante.</param>
+        /// <returns>Un mensaje que indica si la inscripción fue exitosa o si no hay cupos disponibles.</returns>
         public string InscribirEstudianteEnCurso(string codigo)
         {
             if (dictCursos != null)
@@ -159,6 +192,51 @@ namespace BibliotecaCLases.Controlador
             {
                 return "No se encontraron cursos.";
             }
+        }
+
+
+
+
+        /// <summary>
+        /// Obtiene el diccionario de cursos completo.
+        /// </summary>
+        /// <returns>El diccionario de cursos.</returns>
+        public Dictionary<int, Curso> ObtenerDictCursos()
+        {
+            dictCursos = Serializador.LeerJson<Dictionary<int, Curso>>(_path);
+            return dictCursos;
+        }
+
+
+
+
+        /// <summary>
+        /// Obtiene un curso por su código.
+        /// </summary>
+        /// <param name="codigo">El código del curso a obtener.</param>
+        /// <returns>El curso correspondiente o un nuevo objeto Curso si no se encuentra.</returns>
+        public Curso ObtenerCursoPorCodigo(string codigo)
+        {
+            int.TryParse(codigo, out int codigoCurso);
+
+            if (dictCursos.ContainsKey(codigoCurso))
+            {
+                return dictCursos[codigoCurso];
+            }
+            else
+            {
+                return new Curso("", "", "", "");
+            }
+        }
+
+
+        /// <summary>
+        /// Propiedad para obtener o establecer la ruta del archivo JSON de datos.
+        /// </summary>
+        public string Path
+        {
+            get { return _path; }
+            set { _path = value; }
         }
     }
 }
